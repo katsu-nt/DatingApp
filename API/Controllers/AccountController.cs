83df3c2d3fd7c256e74fc88574a47224c2ecb58f
ben <benbilldev@gmail.com>
2025-05-26 18:33:06 +0700

    #4 feat(login): adding authenciation middleware

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,15 +4,16 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AccountController(DataContext context) : BaseApiController
+public class AccountController(DataContext context, ITokenService tokenService) : BaseApiController
 {
     [HttpPost("register")]
-    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
         if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
         using var hmac = new HMACSHA512();
@@ -24,11 +25,15 @@ public class AccountController(DataContext context) : BaseApiController
         };
         context.Users.Add(user);
         await context.SaveChangesAsync();
-        return Ok(user);
+        return Ok(new UserDto
+        {
+            Username = user.UserName,
+            Token = tokenService.CreateToken(user)
+        });
     }
 
     [HttpPost("login")]
-    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
         var user = await context.Users.FirstOrDefaultAsync(x => x.UserName.ToLower() == loginDto.Username.ToLower());
         if (user == null) return Unauthorized("Invalid username");
@@ -42,7 +47,11 @@ public class AccountController(DataContext context) : BaseApiController
                 return Unauthorized("Invalid password");
             }
         }
-        return Ok(user);
+        return Ok(new UserDto
+        {
+            Username = user.UserName,
+            Token = tokenService.CreateToken(user)
+        });
     }
     private async Task<bool> UserExists(string username)
     {
